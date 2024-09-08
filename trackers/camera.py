98d20ce6e7e16e766d6b0ea302d13fc6ce5679f9
Thomas Luechtefeld <tom@insilica.co>
2024-09-08 10:39:37 -0400

    attempt to find an available camera if provided device doesn't work

--- trackers/camera.py
+++ trackers/camera.py
@@ -3,16 +3,42 @@ import cv2
 
 class UserPresentTracker(Tracker):
 
-    def __init__(self, interval_seconds=1, camera_device='/dev/video2'):
+    def __init__(self, interval_seconds=1, camera_device='/dev/video1'):
         super().__init__(interval_seconds)
-        self.cap = cv2.VideoCapture(camera_device)
+        self.camera_device = camera_device
+        self.cap = None
         self.metric = 'duration_at_computer'
         self.duration_at_computer = 0
+
+        # Try to initialize the camera
+        self.initialize_camera()
 
         # Load the pre-trained DNN model
         prototxt_path = 'resources/deploy.prototxt'
         caffemodel_path = 'resources/res10_300x300_ssd_iter_140000.caffemodel'
         self.net = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
+
+    def initialize_camera(self):
+        # List of camera devices to try if the given one fails
+        camera_devices = [self.camera_device] + ['/dev/video{}'.format(i) for i in range(5)]  # Adjust range as needed
+
+        for device in camera_devices:
+            self.cap = cv2.VideoCapture(device)
+            if self.cap.isOpened():
+                ret, _ = self.cap.read()
+                if ret:
+                    print(f"Camera initialized with device: {device}")
+                    return
+                else:
+                    self.cap.release()
+            print(f"Failed to initialize camera with device: {device}")
+
+        raise Exception("Could not find a working camera device.")
+
+    def __del__(self):
+        # Release the capture when the object is destroyed
+        if self.cap:
+            self.cap.release()
 
     def update_metrics(self):
         ret, frame = self.cap.read()
